ed43826e7227e266869df649142a65d366ca7dd5
Rony nogueira <ronyrnds@gmail.com>
2022-07-21 22:14:30 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+import { Box, SimpleGrid, Text  } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <SimpleGrid>
+       <Text color={'button.primary'}>Rony</Text>
+    </SimpleGrid>
+ 
+  )
+}
